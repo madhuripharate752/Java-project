94efe8eb1800691e8062e44b05436eaa79d9812d
sacin pharate <madhuripharate752@gmail.com>
2024-04-08 21:51:39 +0530

    automation array demo

--- src/Arrayexample/ArrayExample.java
+++ src/Arrayexample/ArrayExample.java
@@ -3,13 +3,13 @@ package Arrayexample;
 public class ArrayExample {
     public static void main(String[] args) {
         int [] marks = new int [7];
-        marks[0] = 90;
-        marks [1] = 80;
-        marks[2] = 70;
-        marks[3] = 60;
-        marks[4] = 99;
-        marks [5] = 88;
-        marks [6] = 77;
+        marks[0] = 900;
+        marks [1] = 800;
+        marks[2] = 700;
+        marks[3] = 600;
+        marks[4] = 990;
+        marks [5] = 880;
+        marks [6] = 770;
         for(int i=0; i<7; i++){
             System.out.println(marks[i]);
         }
