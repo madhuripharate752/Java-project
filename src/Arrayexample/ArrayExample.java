0d7c1fb7fb771ccdb5428c7f5720fff22ee0c90a
madhuripharate752 <165996296+madhuripharate752@users.noreply.github.com>
2024-04-13 12:44:58 +0530

    Update ArrayExample.java

--- src/Arrayexample/ArrayExample.java
+++ src/Arrayexample/ArrayExample.java
@@ -2,14 +2,14 @@ package Arrayexample;
 
 public class ArrayExample {
     public static void main(String[] args) {
-        int [] marks = new int [7];
+        int [] marks = new int [9];
         marks[0] = 900;
         marks [1] = 800;
         marks[2] = 700;
         marks[3] = 600;
         marks[4] = 990;
-        marks [5] = 880;
-        marks [6] = 770;
+        marks [5] = 200000;
+        marks [6] = 100000;
         marks [7] = 888880;
         marks [8] = 999990;
         for(int i=0; i<7; i++){
